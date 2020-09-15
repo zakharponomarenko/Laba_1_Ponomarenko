b636681cef55035d7a61cffe56a8dc3ce1f68107
Zakhar Ponomarenko <seloboyz@ukr.net>
2020-09-15 19:31:03 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Laba_IPZ_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("До якого числа рахувати ряд Фібоначі? ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int perv = 1;
+            Console.Write("{0} ", perv);
+            int vtor = 1;
+            Console.Write("{0} ", vtor);
+            int sum = 0;
+
+            while (number >= sum)
+            {
+                sum = perv + vtor;
+
+                Console.Write("{0} ", sum);
+
+                perv = vtor;
+                vtor = sum;
+            }
+        }
+    }
+}
+            
